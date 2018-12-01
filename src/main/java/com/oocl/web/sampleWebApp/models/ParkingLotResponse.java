f5aa449f943b5b93b59201cb99d097e34969bb2b
HOJO3 <joe.ho@oocl.com>
2018-12-01 15:29:11 +0800

    [JoeHo] feat: add feature which can display other details to parkingLotResponse

--- src/main/java/com/oocl/web/sampleWebApp/models/ParkingLotResponse.java
+++ src/main/java/com/oocl/web/sampleWebApp/models/ParkingLotResponse.java
@@ -7,7 +7,10 @@ import com.oocl.web.sampleWebApp.domain.ParkingLot;
 import java.util.Objects;
 
 public class ParkingLotResponse {
+
     private String parkingLotId;
+    private Integer capacity;
+    private Integer availablePositionCount;
 
     public String getParkingLotId() {
         return parkingLotId;
@@ -17,22 +20,48 @@ public class ParkingLotResponse {
         this.parkingLotId = parkingLotId;
     }
 
+    public Integer getCapacity() {
+        return capacity;
+    }
 
-    public static ParkingLotResponse create(String parkingLotId) {
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getAvailablePositionCount() {
+        return availablePositionCount;
+    }
+
+    public void setAvailablePositionCount(Integer availablePositionCount) {
+        this.availablePositionCount = availablePositionCount;
+    }
+
+
+
+//    public static ParkingLotResponse create(String parkingLotId) {
+//        Objects.requireNonNull(parkingLotId);
+//
+//        final ParkingLotResponse response = new ParkingLotResponse();
+//        response.setParkingLotId(parkingLotId);
+//
+//        return response;
+//    }
+
+    public static ParkingLotResponse create(ParkingLot entity) {
+        String parkingLotId = entity.getParkingLotId();
+        Integer capacity = entity.getCapacity();
+        Integer availablePositionCount = entity.getAvailablePositionCount();
+
+
         Objects.requireNonNull(parkingLotId);
 
         final ParkingLotResponse response = new ParkingLotResponse();
         response.setParkingLotId(parkingLotId);
+        response.setCapacity(capacity);
+        response.setAvailablePositionCount(availablePositionCount);
+
 
         return response;
-    }
-
-    public static ParkingLotResponse create(ParkingLot entity) {
-        String parkingLotId = entity.getParkingLotId();
-
-        Objects.requireNonNull(parkingLotId);
-
-        return create(entity.getParkingLotId());
     }
 
     @JsonIgnore
